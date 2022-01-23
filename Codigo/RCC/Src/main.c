b85300802842385074f7fcc5c1959bc6c48305a0
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-01-22 19:36:51 -0500

    CLASE 3

--- Codigo/RCC/Src/main.c
+++ Codigo/RCC/Src/main.c
@@ -18,6 +18,7 @@
 
 #include <stdint.h>
 #include "stm32f4xx.h"
+#include <stdio.h>
 
 //#define AHB1_BASE 	0x40020000UL
 //#define RCC_BASE	(AHB1_BASE + 0x3800U)
@@ -44,10 +45,11 @@ int main(void)
 	systemFreq = SystemCoreClock;
 	/*HSE init*/
 	HSE_Config();
-	systemFreq = SystemCoreClock;
-	/*PLL init*/
-	PLL_Config();
-	systemFreq = SystemCoreClock;
+	printf("CONFIGURACION HSE LISTO->%d\n",systemFreq);
+//	systemFreq = SystemCoreClock;
+//	/*PLL init*/
+//	PLL_Config();
+//	systemFreq = SystemCoreClock;
 //	RCC_CR |= 1U<<18 | 1U<<16;
 //	while(!(RCC_CR & 1U<<17));		//espera que el HSERDY
     /* Loop forever */
@@ -72,31 +74,48 @@ void HSE_Config(void){
 }
 
 
+/**
+ * @brief PLL config
+ * configura el PLL  a 84MHz
+ */
 void PLL_Config(void){
-	/*(opcional) configurar el HSE*/
-
-	//1. cofigurar la fuente de reloj del PLL
-
+	HSE_Config();
+	RCC->CR &=~ (1U<<24);
+	while(RCC->CR & 1U<<25);
+	/*configure PL before enabling it*/
+	//1. PLL clock source
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;	//select HSE CLOCK SOURCE
 	//2. cofigure division factor (M, N, P , Q)
 	/**
-	  * 	f(VCO clock) = f(PLL clock input) Ã— (PLLN / PLLM)	(192 and 432 MHz)
-	  * 	f(PLL general clock output) = f(VCO clock) / PLLP  	<= 84MHz
-	  */
+	 * 	f(VCO clock) = f(PLL clock input) Ã— (PLLN / PLLM)	(192 and 432 MHz)
+	 * 	f(PLL general clock output) = f(VCO clock) / PLLP  	<= 84MHz
+	 */
 	//M
-
+	RCC->PLLCFGR &=~ RCC_PLLCFGR_PLLM;
+	RCC->PLLCFGR |= 4U;							//VCO input frequency (1MHz to 2MHz)
 	//N
-
+	RCC->PLLCFGR &=~ RCC_PLLCFGR_PLLN;
+	RCC->PLLCFGR |= 168U<<6;					//168MHZ
 	//P
-	/*3.Habilitar el PLL*/
-
-	/*4. Cofigurar los prescalers (APB2, APB1)*/
-
-	/*5. configurar la latencia de la flash*/
+	RCC->PLLCFGR &=~ RCC_PLLCFGR_PLLP;
+	RCC->PLLCFGR |= 1U<<16;						//84MHz
+	//3. enable PLL
+	RCC->CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY));			//Waint until
+	/*configure AHBx, APBx precaler*/
+	RCC->CFGR = 0x00000000;						//reset
+	RCC->CFGR |= 0b100<<10;						//100: AHB clock divided by 2
+	/*configure flash latency*/
 	FLASH->ACR &=~ FLASH_ACR_LATENCY;
 	FLASH->ACR |= FLASH_ACR_LATENCY_2WS;
+	/*select system clock source*/
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL));		//wait PLL ready as system clock
+	return;
+}
 
-	/*seleccionar la fuente del sysclk*/
-
-	SystemCoreClockUpdate();
-
+int __io_putchar(int ch){
+	uint8_t c = ch & 0xFF;
+	ITM_SendChar(c);
+	return ch;
 }
