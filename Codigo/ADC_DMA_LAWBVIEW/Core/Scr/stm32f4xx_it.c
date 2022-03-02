f4fa4c1a340e2b7b69f3baefcdf4a3f92328942a
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-03-02 11:29:45 -0500

    ADC update

---
+++ Codigo/ADC_DMA_LAWBVIEW/Core/Scr/stm32f4xx_it.c
@@ -0,0 +1,165 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+#include "USART.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+
+/* Private define ------------------------------------------------------------*/
+
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+/* Private user code ---------------------------------------------------------*/
+
+
+/* External variables --------------------------------------------------------*/
+extern  USART_Handle_t usart2_handle;
+extern void DMA_TrnsmitCpltCallback(void);
+extern void DMA_HalfTransmitCpltCallback(void);
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+
+}
+
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
+void DMA2_Stream4_IRQHandler(void){
+	if(DMA2->HISR & 1U<<5){				//Transfer complete interrupt enable
+		DMA2->HIFCR |= 1U<<5;
+		DMA_TrnsmitCpltCallback();
+	}
+	if(DMA2->HISR & 1U<<4){
+		DMA2->HIFCR |= 1U<<4;
+		DMA_HalfTransmitCpltCallback();
+	}
+	if(DMA2->HISR & 1U<<3){
+		DMA2->HIFCR |= 1U<<3;
+
+	}
+	if(DMA2->HISR & 1U<<2){
+		DMA2->HIFCR |= 1U<<1;
+
+	}
+	return;
+}
+
+void USART2_IRQHandler(void)
+{
+	USART_IRQHandling(&usart2_handle);
+}
+
